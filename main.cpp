4a7824bb3995a9696df2e990065ff907bf7b0ba6
bartoszpyszka <barek@LAPTOP-86VDJFE1>
2025-06-03 22:08:22 +0200

    v0.10 Dodanie komentarzy i usunięcie zbędnych elementów

--- main.cpp
+++ main.cpp
@@ -1,41 +1,42 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <list>
-#include <random>
-#include "GameObject.h"
-#include "Globals.h"
-#include "Player.h"
-#include "Asteroid.h"
 #include "GameLogic.h"
 
 int main()
 {
+    // Zmienne potrzebne do stworzenia okna
     constexpr unsigned int windowWidth = 1200;
     constexpr unsigned int windowHeight = 900;
     const std::string windowTitle = "Space Destroy";
 
-    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), windowTitle, sf::Style::Titlebar | sf::Style::Close);
+    // Tworzy g³ówne okno aplikacji 
+    sf::RenderWindow window(sf::VideoMode(windowWidth, 
+        windowHeight), windowTitle, sf::Style::Titlebar | 
+        sf::Style::Close);
+
+    // Zmienna mierz¹ca czas pomiêdzy klatkami, potrzebna
+    // do obliczania deltaTime
     sf::Clock clock;
 
+    // Inicjalizacja gry
     GameLogic::init();
 
+    // G³ówna pêtla gry. Dzia³a, dopóki 
+    // okno jest otwarte 
     while (window.isOpen()) {
+
+        // Oblicza czas, jaki up³yn¹³ od 
+        // ostatniego odœwie¿enia gry, w sekundach.
+        // Metoda `clock.restart()` resetuje 
+        // zegar i zwraca czas, który up³yn¹³ 
+        // od ostatniego restartu
         float deltaTime = clock.restart().asSeconds();
 
-        // Obs³uga zdarzeñ (zamykanie okna)
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear(sf::Color::Black);
-
+        // GameLogic::update aktualizuje logikê 
+        // gry (np. poruszanie obiektów, obs³ugê 
+        // kolizji, renderowanie).
         GameLogic::update(window, deltaTime);
 
+        // Wyœwietla wszystkie elementy w oknie
         window.display();
     }
 
