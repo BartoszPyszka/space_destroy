636330fe6cd430b721f653edf6d5aad60e1a659a
bartoszpyszka <barek@LAPTOP-86VDJFE1>
2025-06-03 22:10:58 +0200

    v0.10.1 Dodano komentarze

--- GameObject.h
+++ GameObject.h
@@ -1,19 +1,32 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-
+// Klasa bazowa dla wszystkich obiektów gry 
+// (np. gracza, pocisków, asteroid). Udostêpnia wspólne 
+// w³aœciwoœci, takie jak pozycja i k¹t obrotu,
+// oraz metody wirtualne do aktualizacji i renderowania
 class GameObject {
 public:
+    // Konstruktor inicjalizuj¹cy pozycjê i k¹t obrotu obiektu
     GameObject(sf::Vector2f pos, float ang)
         : position(pos), angle(ang) {
     }
 
+    // Wirtualny destruktor – umo¿liwia poprawne 
+    // niszczenie obiektów pochodnych
     virtual ~GameObject() {}
 
+    // Metoda wirtualna – ka¿da klasa dziedzicz¹ca 
+    // musi zaimplementowaæ logikê aktualizacji
     virtual void update(float deltaTime) = 0;
+
+    // Metoda wirtualna – ka¿da klasa dziedzicz¹ca 
+    // musi zaimplementowaæ logikê renderowania
     virtual void render(sf::RenderWindow& window) = 0;
 
-    sf::Vector2f position;   // Pozycja obiektu
-    float angle;             // K¹t obrotu obiektu
+    sf::Vector2f position;   // Pozycja obiektu w przestrzeni gry
+    float angle;             // K¹t obrotu obiektu (w stopniach)
 };
+
+
 
