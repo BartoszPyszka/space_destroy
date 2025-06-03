fc86ffc25a19a5db91531ad158d0f7b33730723b
bartoszpyszka <barek@LAPTOP-86VDJFE1>
2025-06-03 22:12:00 +0200

    v0.10.2 Dodano komenatrze i inicjalizacje

--- GameObject.cpp
+++ GameObject.cpp
@@ -1,1 +1,7 @@
 #include "GameObject.h"
+
+// Inicjalizuje konstruktor klasy GameObject, zawieraj¹cy
+// pozycjê i k¹t obiektu na wartoœci przekazane jako argumenty 
+GameObject::GameObject(sf::Vector2f pos, float ang)
+	: position(pos), angle(ang) {
+}
