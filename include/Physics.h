d2d8b180617e7ff048d1c48e17cc9a080ac77c08
BartoszPyszka <bartoszpyszka@wp.pl>
2025-06-13 23:53:22 +0200

    v1.0.8 Uporządkowanie kodu

--- include/Physics.h
+++ include/Physics.h
@@ -1,9 +1,16 @@
 #pragma once
+
+// Biblioteki zewnętrzne 
 #include <SFML/Graphics.hpp>
 
+// Przestrzeń nazw dla funkcji fizyki kolizji 
 namespace physics {
-	bool intersects(const sf::Vector2f& point, const sf::VertexArray& polygon);  // Kolizja pocisk - asteroida
-	bool intersects(const sf::VertexArray& poly1, const sf::VertexArray& poly2); // Kolizja gracz - asteroida (SAT)
-	sf::VertexArray getTransformed(const sf::VertexArray& polygon, const sf::Transform& transform);
-}
+    // Sprawdza kolizję pocisk - asteroida
+    bool intersects(const sf::Vector2f& point, const sf::VertexArray& polygon);
 
+    // Sprawdza kolizję gracz - asteroida metodą SAT
+    bool intersects(const sf::VertexArray& poly1, const sf::VertexArray& poly2);
+
+    // Zwraca przekształconą tablicę wierzchołków 
+    sf::VertexArray getTransformed(const sf::VertexArray& polygon, const sf::Transform& transform);
+}
